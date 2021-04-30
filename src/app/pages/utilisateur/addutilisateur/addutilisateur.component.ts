f5aa58ec17cf6ca3233fa9d11737c1428bf615d0
TikoKane <tikokane11@gmail.com>
2021-04-30 16:51:37 +0200

    first commit

---
+++ src/app/pages/utilisateur/addutilisateur/addutilisateur.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-addutilisateur',
+  templateUrl: './addutilisateur.component.html',
+  styleUrls: ['./addutilisateur.component.scss']
+})
+export class AddutilisateurComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
