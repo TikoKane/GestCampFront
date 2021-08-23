4b07cebefd2526aaa244838cc943562b6d23f810
TikoKane <tikokane11@gmail.com>
2021-08-23 13:44:53 +0200

    a

--- src/app/pages/campagne/addcampagne/addcampagne.component.ts
+++ src/app/pages/campagne/addcampagne/addcampagne.component.ts
@@ -25,55 +25,55 @@ export class AddcampagneComponent implements OnInit {
   canalenvois;
   regledenvois;
   listeDiffusion;
-  idListeDiffusion='';
-  idModel='';
+  idListeDiffusion = '';
+  idModel = '';
   modeles;
   libelleModele;
   libelleLDD;
   libelleNDV;
   libelleTDC;
-  nomComplet = localStorage.getItem("nom")+" "+localStorage.getItem("prenom");
+  nomComplet = localStorage.getItem("nom") + " " + localStorage.getItem("prenom");
 
-config: NbToastrConfig;
-index = 1;
-destroyByClick = true;
-duration = 2000;
-hasIcon = true;
-position: NbGlobalPosition = NbGlobalPhysicalPosition.TOP_RIGHT;
-preventDuplicates = false;
-status: NbComponentStatus = 'success';
+  config: NbToastrConfig;
+  index = 1;
+  destroyByClick = true;
+  duration = 2000;
+  hasIcon = true;
+  position: NbGlobalPosition = NbGlobalPhysicalPosition.TOP_RIGHT;
+  preventDuplicates = false;
+  status: NbComponentStatus = 'success';
 
-title = 'Ajout d\'une nouvelle campagne !';
-content = `Campagne ajoutée avec succès!`;
+  title = 'Ajout d\'une nouvelle campagne !';
+  content = `Campagne ajoutée avec succès!`;
 
-  constructor(private toastrService: NbToastrService,private campagneService : CampagnesService,private niveauVisibiliteService : NiveauDeVisibilitesService, private typeCampagneService : TypeDeCampagnesService,private canalEnvoiService : CanalEnvoisService,
-    private regleDenvoiService : RegleDEnvoisService, private router: Router,private listeDiffusionService : ListeDeDiffusionsService,private modelService : ModelesService) { }
-  camp : Campagnes = {
-    Code :'Codeeeee',
-    DateDeDebut:'',
-    DateDeFin:'',
-    Description:'',
-    Etat : true,
-    Statut : true,
-    Id:0,
-    IdCanalEnvoi:'',
-    IdEntite:localStorage.getItem('idEntite'),
-    IdNiveauVisibilite:'',
-    IdRegleEnvoi:'',
-    IdTypeCampagne:'',
-    IdUtilisateur:localStorage.getItem('id'),
-    Titre:''
+  constructor(private toastrService: NbToastrService, private campagneService: CampagnesService, private niveauVisibiliteService: NiveauDeVisibilitesService, private typeCampagneService: TypeDeCampagnesService, private canalEnvoiService: CanalEnvoisService,
+    private regleDenvoiService: RegleDEnvoisService, private router: Router, private listeDiffusionService: ListeDeDiffusionsService, private modelService: ModelesService) { }
+  camp: Campagnes = {
+    Code: 'Codeeeee',
+    DateDeDebut: '',
+    DateDeFin: '',
+    Description: '',
+    Etat: true,
+    Statut: true,
+    Id: 0,
+    IdCanalEnvoi: '',
+    IdEntite: localStorage.getItem('idEntite'),
+    IdNiveauVisibilite: '',
+    IdRegleEnvoi: '',
+    IdTypeCampagne: '',
+    IdUtilisateur: localStorage.getItem('id'),
+    Titre: ''
   }
 
-  regleEnvoi : RegleDEnvois = {
-       DateExecution:new Date(),
-       Expediteur:'',
-       Frequence:null,
-       Id : 0,
-       FuseauHoraire :'',
-       NombreTentative :null,
-       Recepteur:'',
-       IdEntite:localStorage.getItem('idEntite'),
+  regleEnvoi: RegleDEnvois = {
+    DateExecution: new Date(),
+    Expediteur: '',
+    Frequence: null,
+    Id: 0,
+    FuseauHoraire: '',
+    NombreTentative: null,
+    Recepteur: '',
+    IdEntite: localStorage.getItem('idEntite'),
   }
   ngOnInit(): void {
     this.niveauVisibiliteService.getAllNiveauDeVisibilite().subscribe((data) => {
@@ -83,110 +83,110 @@ content = `Campagne ajoutée avec succès!`;
       console.log(err);
     });
 
-    
-  this.canalEnvoiService.getAllCanalEnvoi().subscribe((data) => {
-    this.canalenvois = data;
-    console.log(this.canalenvois)
-  }, (err) => {
-    console.log(err);
-  });
+
+    this.canalEnvoiService.getAllCanalEnvoi().subscribe((data) => {
+      this.canalenvois = data;
+      console.log(this.canalenvois)
+    }, (err) => {
+      console.log(err);
+    });
 
 
-  this.typeCampagneService.getAllTypeDeCampagne(localStorage.getItem('idEntite')).subscribe((data) => {
-    this.typecampagnes = data;
-  }, (err) => {
-    console.log(err);
-  });
+    this.typeCampagneService.getAllTypeDeCampagne(localStorage.getItem('idEntite')).subscribe((data) => {
+      this.typecampagnes = data;
+    }, (err) => {
+      console.log(err);
+    });
 
 
-  this.listeDiffusionService.getAllListeDeDiffusion(localStorage.getItem('idEntite')).subscribe((data) => {
-    this.listeDiffusion = data;
-  }, (err) => {
-    console.log(err);
-  });
+    this.listeDiffusionService.getAllListeDeDiffusion(localStorage.getItem('idEntite')).subscribe((data) => {
+      this.listeDiffusion = data;
+    }, (err) => {
+      console.log(err);
+    });
 
-  this.modelService.getAllModele(localStorage.getItem('idEntite')).subscribe((data) => {
-    this.modeles = data;
-    console.log(this.modeles)
-  }, (err) => {
-    console.log(err);
-  });
-
-
-  
-}
+    this.modelService.getAllModele(localStorage.getItem('idEntite')).subscribe((data) => {
+      this.modeles = data;
+      console.log(this.modeles)
+    }, (err) => {
+      console.log(err);
+    });
 
 
 
-onChangeModel(deviceValue) {
-  this.modelService.getModele(deviceValue).subscribe((data) => {
-    this.libelleModele = data['libelle'];
-  }, (err) => {
-    console.log(err);
-  });
-}
-
-onChangeNDV(deviceValue) {
-  this.niveauVisibiliteService.getNiveauDeVisibilite(deviceValue).subscribe((data) => {
-    this.libelleNDV = data['libelle'];
-  }, (err) => {
-    console.log(err);
-  });
-}
-
-onChangeLDD(deviceValue) {
-  this.listeDiffusionService.getListeDeDiffusion(deviceValue).subscribe((data) => {
-    this.libelleLDD = data['titre'];
-  }, (err) => {
-    console.log(err);
-  });
-}
-
-onChangeTDC(deviceValue) {
-  this.typeCampagneService.getTypeDeCampagne(deviceValue).subscribe((data) => {
-    this.libelleTDC = data['libelle'];
-  }, (err) => {
-    console.log(err);
-  });
-}
+  }
 
 
-valider(){
-  this.regleDenvoiService.AddRegleDEnvoi(this.regleEnvoi).subscribe((data) => {
-    this.camp.IdRegleEnvoi=data['id'];
-    this.campagneService.AddCampagne(this.camp).subscribe((data) => {
-      this.campagneService.SendEmail(this.camp,localStorage.getItem('idEntite'),this.idModel).subscribe((data) => {
-        this.ToastValide(this.status,this.title,this.content);
-        this.router.navigate(['/pages/campagne/list']);
+
+  onChangeModel(deviceValue) {
+    this.modelService.getModele(deviceValue).subscribe((data) => {
+      this.libelleModele = data['libelle'];
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  onChangeNDV(deviceValue) {
+    this.niveauVisibiliteService.getNiveauDeVisibilite(deviceValue).subscribe((data) => {
+      this.libelleNDV = data['libelle'];
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  onChangeLDD(deviceValue) {
+    this.listeDiffusionService.getListeDeDiffusion(deviceValue).subscribe((data) => {
+      this.libelleLDD = data['titre'];
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  onChangeTDC(deviceValue) {
+    this.typeCampagneService.getTypeDeCampagne(deviceValue).subscribe((data) => {
+      this.libelleTDC = data['libelle'];
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+
+  valider() {
+    this.regleDenvoiService.AddRegleDEnvoi(this.regleEnvoi).subscribe((data) => {
+      this.camp.IdRegleEnvoi = data['id'];
+      this.campagneService.AddCampagne(this.camp).subscribe((data) => {
+        this.campagneService.SendEmail(this.camp, localStorage.getItem('idEntite'), this.idModel).subscribe((data) => {
+          this.ToastValide(this.status, this.title, this.content);
+          this.router.navigate(['/pages/campagne/list']);
+        }, (err) => {
+          console.log(err);
+        });
       }, (err) => {
         console.log(err);
       });
     }, (err) => {
       console.log(err);
     });
-  }, (err) => {
-    console.log(err);
-  });
-  
-}
+
+  }
 
 
-private ToastValide(type: NbComponentStatus, title: string, body: string) {
-  const config = {
-    status: type,
-    destroyByClick: this.destroyByClick,
-    duration: this.duration,
-    hasIcon: this.hasIcon,
-    position: this.position,
-    preventDuplicates: this.preventDuplicates,
-  };
-  const titleContent = title ? `${title}` : '';
+  private ToastValide(type: NbComponentStatus, title: string, body: string) {
+    const config = {
+      status: type,
+      destroyByClick: this.destroyByClick,
+      duration: this.duration,
+      hasIcon: this.hasIcon,
+      position: this.position,
+      preventDuplicates: this.preventDuplicates,
+    };
+    const titleContent = title ? `${title}` : '';
 
-  this.index += 1;
-  this.toastrService.show(
-    body,
-    `${titleContent}`,
-    config);
-}
+    this.index += 1;
+    this.toastrService.show(
+      body,
+      `${titleContent}`,
+      config);
+  }
 }
 
